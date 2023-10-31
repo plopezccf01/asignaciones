efca93e5d0ecbe491cf5c2d4419eb072b068e215
Pablo López <pablo.lopez@eurotransportcar.com>
2023-10-31 12:57:43 +0100

    Modified user bundle

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -3,11 +3,30 @@
 namespace UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
+    /**
+     * Función que renderiza la página principal del módulo de usuarios
+     * 
+     * @author Pablo López <pablo.lopez@eurotransportcar.com>
+     * @return Response
+     */
     public function indexAction()
     {
         return $this->render('UserBundle:Default:index.html.twig');
+    }
+
+    /**
+     * Función que renderiza una página con el nombre de la URL
+     * 
+     * @author Pablo López <pablo.lopez@eurotransportcar.com>
+     * @param   string $name
+     * @return  Response
+     */
+    public function exampleAction($name)
+    {
+        return $this->render('UserBundle:Default:index.html.twig', array('name' => $name));
     }
 }
