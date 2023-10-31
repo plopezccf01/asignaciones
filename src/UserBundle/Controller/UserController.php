409962942d4f52b99e6eb90a695f2ecf763b95c6
Pablo López <pablo.lopez@eurotransportcar.com>
2023-10-31 13:31:55 +0100

    Created entity user

---
+++ src/UserBundle/Controller/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+    /**
+     * Función que renderiza la página principal del módulo de usuarios
+     * 
+     * @author Pablo López <pablo.lopez@eurotransportcar.com>
+     * @return Response
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $users = $em->getRepository('UserBundle:User')->findAll();
+
+        $res = 'Lista de usuarios: <br />';
+
+        foreach ($users as $user) {
+            $res .= 'Usuario: ' . $user->getUsername() . ' - Email: ' . $user->getEmail() . '<br />';
+        }
+
+        return new Response($res);
+    }
+
+    public function viewAction($username) {
+        $repository = $this->getDoctrine()->getRepository('UserBundle:User');
+
+        // $user = $repository->find($id);
+
+        $user = $repository->findOneByUsername($username);
+
+        return new Response('Usuario: ' . $user->getUsername() . ' con email: ' . $user->getEmail());
+    }
+}
