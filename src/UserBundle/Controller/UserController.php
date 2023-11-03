4be52e0363a4bade3c6a74eab158beaa26d0be1e
Pablo López <pablo.lopez@eurotransportcar.com>
2023-11-03 13:12:08 +0100

    Created a edit form from users

--- src/UserBundle/Controller/UserController.php
+++ src/UserBundle/Controller/UserController.php
@@ -72,6 +72,81 @@ class UserController extends Controller
         return $this->render('UserBundle:User:add.html.twig', array('form' => $form -> createView()));
     }
 
+    public function editAction($id) {
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('UserBundle:User')->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        $form = $this->createEditForm($user);
+
+        return $this->render('UserBundle:User:edit.html.twig', array('user' => $user, 'form' => $form->createView()));
+    }
+
+    private function createEditForm(User $entity) {
+        $form = $this->createForm(new UserType(), $entity, 
+            array('action' => $this->generateUrl('user_update', array('id' => $entity->getId())), 'method' => 'PUT'));
+
+        return $form;
+    }
+
+    
+    public function updateAction($id, Request $request) {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $em->getRepository('UserBundle:User')->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        $form = $this->createEditForm($user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password =$form->get('password')->getData();
+            if (!empty($password)) {
+                $encoder = $this->container->get('security.password_encoder');
+                $encoded = $encoder->encodePassword($user, $password);
+                $user->setPassword($encoded);
+            } else {
+                $recoverPass = $this->recoverPass($id);
+                $user->setPassword($recoverPass[0]['password']);
+            }
+
+            $em->flush();
+
+            return $this->redirectToRoute('user_edit', array('id' => $user->getId()));
+        }
+
+        return $this->render('UserBundle:User:edit.html.twig', array('user' => $user, 'form' => $form->createView()));
+    }
+
+    /**
+     * Función que recupera el password del usuario
+     *
+     * @param $id
+     * @return void
+     */
+    private function recoverPass($id) {
+       
+        /** @var $em EntityManager */
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery(
+            'SELECT u.password
+            FROM UserBundle:User u
+            WHERE u.id = :id'
+        )->setParameter('id', $id);
+
+        $currentPass = $query->getResult();
+
+        return $currentPass;
+    }
+
     /**
      * Función que muestra un usuario determinado según su ID
      * 
